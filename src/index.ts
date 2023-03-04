23edae2bdf60c5d2a3f5a8d9714a0d9d6bce71ee
Noureldin2303 <faragnour18@gmail.com>
2023-03-04 23:42:03 +0200

    first commit initiating server

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import express, { Application, Request, Response } from 'express'
+import morgan from 'morgan'
+import helmet from 'helmet'
+
+const PORT = 5000
+
+// create instance server
+const app: Application = express()
+
+// middlewares
+// parsing incoming requests
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+// logger middleware
+app.use(morgan('dev'))
+// security
+app.use(helmet())
+
+//add routea
+app.get('/', (_req: Request, res: Response) => {
+  res.json('Hello Server! 🚀')
+})
+
+app.use((_req: Request, res: Response) => {
+  res.status(404).json('Whoops!! You are lost go back to documentation to find your way back to Home again 😂')
+})
+
+//start express server
+app.listen(PORT, () => {
+  console.log(`Server is starting at port:${PORT}`)
+})
+
+export default app
