40af6f92288c6758755e35c360561dfecfc341de
Noureldin2303 <faragnour18@gmail.com>
2023-03-04 23:53:12 +0200

    bcrypt / jwt / config /authUser method

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express'
 import User from '../models/user.model'
-
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import config from '../config'
 
 const createUser = async (req: Request, res: Response) => {
   try {
@@ -10,9 +12,41 @@ const createUser = async (req: Request, res: Response) => {
 
     if (ChkUser) return res.status(400).json('user already exists')
 
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(req.body.password, salt)
+    user.password = hashedPassword
+    
     await user.save()
 
+    const token = jwt.sign({ _id: user }, config.JWT_SECERT as string, {
+      expiresIn: config.EXPIRES_IN,
+    })
+
+    res.header('x-auth-header', token)
+
     res.status(200).json('user added successfully')
+  } catch (err) {
+    res.status(500).json({ message: 'Internal server error: ', err })
+  }
+}
+
+const authUser = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body
+
+    const user = (await User.findOne({ email: email })) as any
+
+    if (!user) return res.status(404).json('user credentionals')
+
+    if (!bcrypt.compareSync(password, user.password))
+      return res.status(404).json('Wrong credentionals')
+
+    const token = jwt.sign({ _id: user }, config.JWT_SECERT as string, {
+      expiresIn: config.EXPIRES_IN,
+    })
+
+    res.header('x-auth-header', token)
+
   } catch (err) {
     res.status(500).json({ message: 'Internal server error: ', err })
   }
@@ -43,5 +77,6 @@ const getSpecificUser = async (req: Request, res: Response) => {
 export default {
   createUser,
   getAllusers,
-  getSpecificUser
+  getSpecificUser,
+  authUser,
 }
